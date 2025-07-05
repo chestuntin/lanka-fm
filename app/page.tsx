7f974fe0a554a4b20358f886848146b1255488d6
chestuntin <chestuntin@gmail.com>
2025-07-05 16:03:12 +0200

    Move carousel navigation arrows further outside the container

--- app/page.tsx
+++ app/page.tsx
@@ -39,8 +39,8 @@ export default function HomePage() {
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className="-left-8" />
-          <CarouselNext className="-right-8" />
+          <CarouselPrevious className="-left-14" />
+          <CarouselNext className="-right-14" />
         </Carousel>
       </div>
     </div>
