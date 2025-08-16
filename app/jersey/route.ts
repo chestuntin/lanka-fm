c936aded305ac55294a097dc53ad0c4231d9c421
chestuntin <chestuntin@gmail.com>
2025-08-16 17:23:11 +0200

    Added jersey redirect route

---
+++ app/jersey/route.ts
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+
+export async function GET() {
+  redirect(
+    "https://mooseclothingcompany.com/shop/srilanka-cricket-clothing/cricket-jersey/odi-fan-jersey-mens-national-zimbabwe-tour-2025/"
+  );
+}
